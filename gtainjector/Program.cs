b26c46499a6a305c794b6bca341dc01682ee9d2a
vape <vapeownsu@gmail.com>
2023-11-02 22:26:18 +0200

    Add files via upload

---
+++ gtainjector/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+public class injector
+{
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, int dwSize, int flAllocationType, int flProtect);
+
+    [DllImport("kernel32.dll")]
+    public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, string lpBuffer, int nSize, out int lpNumberOfBytesWritten);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttribute, int dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, int dwCreationFlags, IntPtr lpThreadId);
+
+    public static void InjectDll()
+    {
+        string processName = "GTA5";
+        string dllPath = "C:\\yimmenuinjector\\yimmenu.dll";
+
+        Process[] processes = Process.GetProcessesByName(processName);
+
+        if (processes.Length == 0)
+        {
+            Console.WriteLine($"No process named {processName} found.");
+            return;
+        }
+
+        int processId = processes[0].Id;
+
+        IntPtr hProcess = OpenProcess(0x1F0FFF, false, processId);
+        if (hProcess == IntPtr.Zero)
+        {
+            Console.WriteLine("Failed to open process.");
+            return;
+        }
+
+        IntPtr dllPathAddr = VirtualAllocEx(hProcess, IntPtr.Zero, dllPath.Length, 0x1000, 0x40);
+        if (dllPathAddr == IntPtr.Zero)
+        {
+            Console.WriteLine("Failed to allocate memory in the remote process.");
+            return;
+        }
+
+        int bytesWritten;
+        if (!WriteProcessMemory(hProcess, dllPathAddr, dllPath, dllPath.Length, out bytesWritten))
+        {
+            Console.WriteLine("Failed to write the DLL path to the remote process.");
+            return;
+        }
+
+        IntPtr loadLibraryAddr = GetLoadLibraryAddress();
+        IntPtr hThread = CreateRemoteThread(hProcess, IntPtr.Zero, 0, loadLibraryAddr, dllPathAddr, 0, IntPtr.Zero);
+        if (hThread == IntPtr.Zero)
+        {
+            Console.WriteLine("Failed to create a remote thread.");
+            return;
+        }
+
+        Console.WriteLine("DLL injection successful!");
+    }
+
+    private static IntPtr GetLoadLibraryAddress()
+    {
+        IntPtr kernel32 = GetModuleHandle("kernel32.dll");
+        if (kernel32 == IntPtr.Zero)
+        {
+            return IntPtr.Zero;
+        }
+
+        return GetProcAddress(kernel32, "LoadLibraryA");
+    }
+
+    public static void Main()
+    {
+        InjectDll();
+    }
+}
+
+
---
+++ yimmenu-injector/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Net;
+using System.IO;
+using Figgle;
+using System.Runtime.InteropServices;
+using System.Text;
+
+Console.WriteLine(FiggleFonts.Standard.Render("yim menu injector"));
+Console.WriteLine("injector made by vape");
+Console.WriteLine("cheat made by yimura");
+Console.WriteLine("downloading assets...");
+string dir = @"c:/yimmenuinjector";
+if (!Directory.Exists(dir))
+{
+    Directory.CreateDirectory(dir);
+}
+WebClient webClient = new WebClient();
+webClient.DownloadFile("https://github.com/YimMenu/YimMenu/releases/download/nightly/YimMenu.dll", @"c:/yimmenuinjector/yimmenu.dll");
+webClient.DownloadFile("https://hyperion.cat/gtainjector.exe", @"c:/yimmenuinjector/gtainjector.exe");
+Console.WriteLine("extracting...");
+Thread.Sleep(2000);
+String ZipPath = @"c:/yimmenuinjector/injector.zip";
+String extractPath = @"c:/yimmenuinjector";
+File.Delete("c:/yimmenuinjector/injector.zip");
+Console.WriteLine("injecting...");
+Thread.Sleep(2000);
+Process.Start("c:/yimmenuinjector/gtainjector.exe");
