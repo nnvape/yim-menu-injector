e99c52c71d674b1e84543359d072af8933f7a064
vape <vapeownsu@gmail.com>
2023-11-03 12:53:09 +0200

    enhanced

--- yimmenu-injector/Program.cs
+++ yimmenu-injector/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.IO.Compression;
 using System.Net;
@@ -10,16 +10,45 @@ using System.Text;
 Console.WriteLine(FiggleFonts.Standard.Render("yim menu injector"));
 Console.WriteLine("injector made by vape");
 Console.WriteLine("cheat made by yimura");
-Console.WriteLine("downloading assets...");
-string dir = @"c:/yimmenuinjector";
-if (!Directory.Exists(dir))
+Console.WriteLine("should GTA V steam edition launch? (y/n)");
+
+string userInput = Console.ReadLine();
+
+if (userInput.ToUpper() == "Y")
 {
+    Process p = new Process();
+    p.StartInfo.FileName = "C:\\Program Files (x86)\\Steam\\steam.exe";
+    p.StartInfo.Arguments = "steam://rungameid/271590";
+    p.Start();
+    Thread.Sleep(30000);
+    Console.WriteLine("downloading assets...");
+    string dir = @"c:/yimmenuinjector";
+    if (!Directory.Exists(dir))
+    {
     Directory.CreateDirectory(dir);
+    }
+    WebClient webClient = new WebClient();
+    webClient.DownloadFile("https://github.com/YimMenu/YimMenu/releases/download/nightly/YimMenu.dll", @"c:/yimmenuinjector/yimmenu.dll");
+    webClient.DownloadFile("https://hyperion.cat/gtainjector.exe", @"c:/yimmenuinjector/gtainjector.exe");
+    Thread.Sleep(2000);
+    Console.WriteLine("injecting...");
+    Thread.Sleep(2000);
+    Process.Start("c:/yimmenuinjector/gtainjector.exe");
 }
-WebClient webClient = new WebClient();
-webClient.DownloadFile("https://github.com/YimMenu/YimMenu/releases/download/nightly/YimMenu.dll", @"c:/yimmenuinjector/yimmenu.dll");
-webClient.DownloadFile("https://hyperion.cat/gtainjector.exe", @"c:/yimmenuinjector/gtainjector.exe");
-Thread.Sleep(2000);
-Console.WriteLine("injecting...");
-Thread.Sleep(2000);
-Process.Start("c:/yimmenuinjector/gtainjector.exe");
+if (userInput.ToUpper() == "N")
+{
+    Console.WriteLine("downloading assets...");
+    string dir = @"c:/yimmenuinjector";
+    if (!Directory.Exists(dir))
+    {
+        Directory.CreateDirectory(dir);
+    }
+    WebClient webClient = new WebClient();
+    webClient.DownloadFile("https://github.com/YimMenu/YimMenu/releases/download/nightly/YimMenu.dll", @"c:/yimmenuinjector/yimmenu.dll");
+    webClient.DownloadFile("https://hyperion.cat/gtainjector.exe", @"c:/yimmenuinjector/gtainjector.exe");
+    Thread.Sleep(2000);
+    Console.WriteLine("injecting...");
+    Thread.Sleep(2000);
+    Process.Start("c:/yimmenuinjector/gtainjector.exe");
+}
+
