de2607991cb65dcce7afdf96b1a93db6ab9fd6a5
vape <vapeownsu@gmail.com>
2023-12-27 10:29:03 +0200

    Update Program.cs

--- yimmenu-injector/Program.cs
+++ yimmenu-injector/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.IO.Compression;
 using System.Net;
@@ -13,26 +13,14 @@ Console.WriteLine("cheat made by yimura");
 Console.WriteLine("should GTA V steam edition launch? (y/n)");
 
 string userInput = Console.ReadLine();
-string appName = "GTA5";
-string directoryPath = "C:\\yimmenuinjector\\";
-string extractPath = @"c:/yimmenuinjector";
-String ZipPath = @"c:/yimmenuinjector/binary.zip";
-DirectoryInfo directory = new DirectoryInfo(directoryPath);
-const string dir = @"c:/yimmenuinjector";
-if (!Directory.Exists(dir))
-    Directory.CreateDirectory(dir);
+DirectoryInfo directory = new DirectoryInfo("C:\\yimmenuinjector\\");
+if (!Directory.Exists("c:/yimmenuinjector"))
+    Directory.CreateDirectory("c/yimmenuinjector");
 using (var client = new WebClient())
 {
-    client.DownloadFile("https://nightly.link/YimMenu/YimMenu/workflows/ci/master/binary.zip", @"c:/yimmenuinjector/binary.zip");
-    ZipFile.ExtractToDirectory(ZipPath, extractPath);
-    File.Delete("c:/yimmenuinjector/binary.zip");
     File.Delete("c:/yimmenuinjector/yimmenu.dll");
-    foreach (var file in directory.GetFiles("*.dll"))
-    {
-        string newName = Path.Combine(file.DirectoryName, "yimmenu" + file.Extension);
-        file.MoveTo(newName);
-    }
-
+    File.Delete("c:/yimmenuinjector/gtainjector.exe");
+    client.DownloadFile("https://github.com/YimMenu/YimMenu/releases/download/nightly/YimMenu.dll", @"c:/yimmenuinjector/yimmenu.dll");
     client.DownloadFile("https://github.com/nnvape/yim-menu-injector/releases/download/download/gtainjector.exe", @"c:/yimmenuinjector/gtainjector.exe");
 }
 
@@ -52,7 +40,7 @@ if (userInput.ToUpper() == "Y")
     Process[] processes;
     do
     {
-        processes = Process.GetProcessesByName(appName);
+        processes = Process.GetProcessesByName("GTA5");
         if (processes.Length > 0)
         {
             Console.WriteLine("injecting...");
